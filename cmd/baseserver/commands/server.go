5d10514746bc0bbadd27c13c19e53b6e0c5d32e0
SON <son@SONs-Mac-mini.local>
2022-11-29 17:29:31 +0700

    root cmd

---
+++ cmd/baseserver/commands/server.go
@@ -0,0 +1,71 @@
+package commands
+
+import (
+	"net"
+	"os"
+	"os/signal"
+	"runtime/debug"
+	"syscall"
+
+	"com.son.server.base/shared/mlog"
+	"github.com/spf13/cobra"
+)
+
+var serverCmd = &cobra.Command{
+	Use:          "server",
+	Short:        "Run the Gobase server",
+	RunE:         serverCmdF,
+	SilenceUsage: true,
+}
+
+func init() {
+	RootCmd.AddCommand(serverCmd)
+	RootCmd.RunE = serverCmdF
+}
+
+func serverCmdF(command *cobra.Command, args []string) error {
+	interruptChan := make(chan os.Signal, 1)
+	return runServer(interruptChan)
+}
+
+func runServer(interruptChan chan os.Signal) error {
+	// Setting the highest traceback level from the code.
+	// This is done to print goroutines from all threads (see golang.org/issue/13161)
+	// and also preserve a crash dump for later investigation.
+	debug.SetTraceback("cash")
+
+	notifyReady()
+
+	// wait for kill signal before attempting to gracefully shutdown
+	// the running service
+	signal.Notify(interruptChan, syscall.SIGINT, syscall.SIGTERM)
+	return nil
+}
+
+func notifyReady() {
+	// If the environment vars provide a systemd notification socket,
+	// notify systemd that the server is ready.
+	systemdSocket := os.Getenv("NOTIFY_SOCKET")
+	if systemdSocket != "" {
+		mlog.Info("Sending systemd READY notification.")
+
+		if err := sendSystemdReadyNotification(systemdSocket); err != nil {
+			mlog.Error(err.Error())
+		}
+	}
+}
+
+func sendSystemdReadyNotification(socketPath string) error {
+	msg := "READY=1"
+	addr := &net.UnixAddr{
+		Name: socketPath,
+		Net:  "unixgram",
+	}
+	conn, err := net.DialUnix(addr.Net, nil, addr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	_, err = conn.Write([]byte(msg))
+	return err
+}
--- go.mod
+++ go.mod
@@ -2,8 +2,15 @@ module com.son.server.base
 
 go 1.19
 
+require github.com/spf13/cobra v1.6.1
+
 require (
+	github.com/francoispqt/gojay v1.2.13 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
-	github.com/spf13/cobra v1.6.1 // indirect
+	github.com/mattermost/logr/v2 v2.0.15 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/wiggin77/merror v1.0.2 // indirect
+	github.com/wiggin77/srslog v1.0.1 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
